e8a1f2ae0812c3f0ad10bd4788802181908f597c
Harry Park <harry@harrypark.io>
2017-01-11 14:48:02 +1100

    webUI

--- validation.py
+++ validation.py
@@ -9,14 +9,14 @@ msg = ["0 0", "0 0"]
 def getValidDab(position):
     bend = [m[0] == 1 for m in msg]
     pitch = [m[2:] for m in msg]
-    
+
     if position[1] == "l":
         if bend[0] or not bend[1]:
             return False
     else:
         if not bend[0] or bend[1]:
             return False
-    
+
     if position[0] == "b":
         return pitch[0] > 20 and pitch[1] > 20
     else:
--- webUI/pre/assets/js/main.js
+++ webUI/pre/assets/js/main.js
@@ -2,8 +2,38 @@ $tl = $('#tl');
 $tr = $('#tr');
 $bl = $('#bl');
 $br = $('#br');
-
 $(document).ready(function() {
-  $br.addClass('visible');
+	$ftl;
+	$ftr;
 });
 // $br.removeClass('visible');
+
+function tr() {
+	rmvall();
+	$tr.addClass('visible');
+}
+
+function br() {
+	rmvall();
+	$br.addClass('visible');
+}
+
+function bl() {
+	rmvall();
+	$bl.addClass('visible');
+}
+
+function tl() {
+	rmvall();
+	$tl.addClass('visible');
+}
+
+function rmvall() {
+	$tl.removeClass('visible');
+	$tr.removeClass('visible');
+	$bl.removeClass('visible');
+	$br.removeClass('visible');
+}
+
+
+// .delay(1000)
