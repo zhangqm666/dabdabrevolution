27b005a9879479689fdb8afd09e70428619f2a99
Harry Park <harry@harrypark.io>
2017-01-11 20:10:07 +1100

    dabweb - added keyboard control

--- webUI/pre/assets/js/main.js
+++ webUI/pre/assets/js/main.js
@@ -2,11 +2,61 @@ $tl = $('#tl');
 $tr = $('#tr');
 $bl = $('#bl');
 $br = $('#br');
-$(document).ready(function() {
-	$ftl;
-	$ftr;
-});
+
+$tick = $('#tick');
+$cross = $('#cross');
+
 // $br.removeClass('visible');
+
+
+
+$(document).keyup(function(e) {
+	if (e.keyCode == 37) { // 37 == left arrow
+		tl();
+	} else if (e.keyCode == 38) { // 38 == up arrow
+		tr();
+	} else if (e.keyCode == 39) { // 39 == right arrow
+		br();
+	} else if (e.keyCode == 40) { // 40 == down arrow
+		bl();
+	} else if (e.keyCode == 88) { // 88 == x
+		cross();
+	} else if (e.keyCode == 86) { // 86 == v
+		tick();
+	}  else if (e.keyCode == 67) { // 67 == c
+		rmvall();
+	}
+});
+
+
+
+// $('#btn-tl').on('click', function() {
+// 	tl();
+// });
+
+// $('#btn-cl').on('click', function() {
+// 	rmvall();
+// });
+
+// $('#btn-tr').on('click', function() {
+// 	tr();
+// });
+
+// $('#btn-bl').on('click', function() {
+// 	bl();
+// });
+
+// $('#btn-br').on('click', function() {
+// 	br();
+// });
+
+// $('#btn-tick').on('click', function() {
+// 	tick();
+// });
+
+// $('#btn-cross').on('click', function() {
+// 	cross();
+// });
 
 function tr() {
 	rmvall();
@@ -28,11 +78,23 @@ function tl() {
 	$tl.addClass('visible');
 }
 
+function tick() {
+	rmvall();
+	$tick.addClass('visible');
+}
+
+function cross() {
+	rmvall();
+	$cross.addClass('visible');
+}
+
 function rmvall() {
 	$tl.removeClass('visible');
 	$tr.removeClass('visible');
 	$bl.removeClass('visible');
 	$br.removeClass('visible');
+	$tick.removeClass('visible');
+	$cross.removeClass('visible');
 }
 
 
